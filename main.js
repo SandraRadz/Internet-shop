224e7dec0a1586bf5cc5d1d76f6b7ce071370556
SandraRadz <32108852+SandraRadz@users.noreply.github.com>
2017-10-10 23:02:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ $(document).ready(function(){
     var $bought = $(".list_of_bought");
     var label = $(".one_bought_good").html();
     var $new_input = $("#big_text_area");
+    var $buttom_buy = $(".buy");
 
 
     function addItem(title){
-        if(document.getElementById("big_text_area").value != "") {
-
+        if($("#big_text_area").value != "") {
             var $node = $(SEGMENT);
             var $lab_node = $(label);
             var count = 1;
 
             $node.find(".list_to_buy").text(title);
             $lab_node.find(".name_of_bought_good").text(title);
+
 
             $seg_count = $node.find(".count_of_good");
             $lab_count = $lab_node.find(".count_of_bought_good");
@@ -25,20 +26,18 @@ $(document).ready(function(){
             $seg_count.text(count);
             $lab_count.text(count);
 
-            $not_bought.append($lab_node);
-            $list.append($node);
-
             $node.find(".plus").click(function(){
                 count += 1;
+                alert(count);
                 $seg_count.text(count);
                 $lab_count.text(count);
             });
 
             $node.find(".minus").click(function(){
-               if(count>1){ count -= 1;
-                $seg_count.text(count);
-                $lab_count.text(count);
-               }
+                if(count>1){ count -= 1;
+                    $seg_count.text(count);
+                    $lab_count.text(count);
+                }
             });
 
             $node.find(".cross").click(function(){
@@ -48,16 +47,24 @@ $(document).ready(function(){
 
             $node.find(".buy").click(function(){
                 $bought.append($lab_node);
+                $buttom_buy.hide();
+
 
             });
 
+            $not_bought.append($lab_node);
+            $list.append($node);
+
             document.getElementById("big_text_area").value = null;
-        }}
+        }
+    }
 
     $(".add").click(function(){
         var new_name = $new_input.val();
         addItem(new_name);
     });
 
-
+    addItem("Помідори");
+    addItem("Cup");
+    addItem("Масло");
 });
