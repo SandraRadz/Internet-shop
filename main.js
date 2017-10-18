4b378c0dbefb2ab39c5fc33cf931d1509b686751
SandraRadz <32108852+SandraRadz@users.noreply.github.com>
2017-10-18 04:54:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -34,6 +34,11 @@ $(document).ready(function(){
                     count += 1;
                     $node.find(".count_of_good").text(count);
                     $lab_node.find(".count_of_bought_good").text(count);
+                    if(count>1) {
+                        $node.find(".minus").css('background-color', 'red');
+                    }
+
+
                 });
 
                 $node.find(".minus").click(function(){
@@ -41,6 +46,9 @@ $(document).ready(function(){
                         count -= 1;
                         $node.find(".count_of_good").text(count);
                         $lab_node.find(".count_of_bought_good").text(count);
+                    }
+                    if(count<2) {
+                        $node.find(".minus").css('background-color', 'lightcoral');
                     }
                 });
 
@@ -56,7 +64,7 @@ $(document).ready(function(){
                         $node.find('.buy').text('Не куплено');
                         $node.find('.cross').hide();
                         $node.find('.list_to_buy').css('text-decoration', 'line-through');
-
+                        $node.find('.tooltip').attr('data-tooltip', 'Ще не купили');
                         $bought.append($lab_node);
                         bought = true;
                     } else {
@@ -65,25 +73,27 @@ $(document).ready(function(){
                         $node.find('.buy').text('Купити');
                         $node.find('.cross').show();
                         $node.find('.list_to_buy').css('text-decoration', 'none');
+                        $node.find('.tooltip').attr('data-tooltip', 'Купити товар');
                         $not_bought.append($lab_node);
                         bought = false;
                     }
                     });
 
               $node.find(".list_to_buy").click(function () {
-                  $node.find('.list_to_buy').hide();
-                  $node.find('.change').show();
-                  $node.find('.change').focus();
-                  $node.find(".change").val($node.find('.list_to_buy').text());
-                  $node.find('.change').css('font-size', '12px');
+                  if (!bought) {
+                      $node.find('.list_to_buy').hide();
+                      $node.find('.change').show();
+                      $node.find('.change').focus();
+                      $node.find(".change").val($node.find('.list_to_buy').text());
+                      $node.find('.change').css('font-size', '12px');
 
-                  $(document).mouseup(function (e) {
-                      $node.find('.list_to_buy').show();
-                      $node.find('.change').hide();
-                      $node.find(".list_to_buy").text($node.find('.change').val());
-                      $lab_node.find(".name_of_bought_good").text($node.find('.change').val());
-                  });
-
+                      $(document).mouseup(function (e) {
+                          $node.find('.list_to_buy').show();
+                          $node.find('.change').hide();
+                          $node.find(".list_to_buy").text($node.find('.change').val());
+                          $lab_node.find(".name_of_bought_good").text($node.find('.change').val());
+                      });
+                  }
               });
 
                 $('.big_text_area').focus();
@@ -99,7 +109,6 @@ $(document).ready(function(){
         addItem(new_name);
 
     });
-
     $('html').keydown(function(eventObject){
         if (eventObject.keyCode == 13) {
             var new_name = $new_input.val();
